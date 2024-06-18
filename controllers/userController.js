c0325a95d6c64130f3fd5f5268df7e8edaf95319
Tomas Roa <tomas.roa@gmail.com>
2024-06-18 11:36:38 -0400

    se crea gastos controller y corrigen errores

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,43 @@
 import path from "path";
+import { addRoommateQuery, getRoommatesQuery } from "../models/userQueries.js";
+import { recalcularMontoGastos } from "../models/gastosQueries.js";  
 import axios from "axios";
-import { v4 as uuidv4 } from "uuid";
-import  { addRoomMateQuery } from "../models/userQueries.js";
+import { v4 as uuidv4 } from 'uuid';  
 const __dirname = path.resolve();
-
-const apiUrl = "https://api.example.com/users";
+const apiUrl = "https://randomuser.me/api/";
 
 export const home = (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/index.html"));
+    res.sendFile(__dirname + "/views/index.html");
 }
 
-export const addRoomMate = async (req, res) => {
+export const addRoommate = async (req, res) => {
     try {
-        const res = await axios.get(apiUrl); 
-        const data = res.data.results[0];
-        const id = uuidv4().slice(0, 4);
-        const newRoomie = {
-            id,
-            nombre: `${data.name.first} ${data.name.last}`,
-            email: data.email,
-            debe: 0,
-            recibe: 0
+        const response = await axios.get(apiUrl);
+        const randomUser = response.data.results[0];
+        const id = uuidv4().slice(0, 8);
+        const newUser = {
+          id,
+          nombre: `${randomUser.name.first} ${randomUser.name.last}`,
+          email: randomUser.email,
+          debe: 0,
+          recibe: 0,
         };
 
-        const results = await addRoomMateQuery(newRoomie);
+
+        const results = await addRoommateQuery(newUser);
+        recalcularMontoGastos();
         res.status(201).json(results);
     } catch (error) {
-        console.log('Error: ', error.code,'Error Message:', error.message);
+        res.status(500).json({ error: error.message });
     }
 }
 
-
+export const getRoommates = async (req, res) => {
+    try {
+        const results = await getRoommatesQuery();
+        res.status(200).json(results);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
 
